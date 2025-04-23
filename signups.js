332ea4d3a15605b4f7843514e07280d1662faad0
HiralGarg05 <hiralgarg0501@gmail.com>
2025-04-23 19:14:26 +0530

    Signups is uploaded

---
+++ signups.js
@@ -0,0 +1,8 @@
+function showAlert() {
+    document.getElementById('alertBox').style.display = 'block';
+}
+
+// Function to close the alert box
+function closeAlert() {
+    document.getElementById('alertBox').style.display = 'none';
+}
